462acb05059107756ca6727f45fff1c16bbcf236
Stefan Keller <aegiskeller@gmail.com>
2025-03-07 01:02:10 +0000

    struggle with index is real

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,4 +2,29 @@
 # Install astrometry.net 
 sudo apt-get update
 sudo apt-get install -y astrometry.net 
-sudo apt-get install astrometry-data-tycho2
+# this will install the index files for the astrometry.net
+# in the directory /usr/share/astrometry
+# although tycho2 files suffice for some images
+# it could be the case we need to install more index files
+# sudo apt-get install astrometry-data-tycho2
+# the 4000 series index files
+cd /usr/share/astrometry
+for ((i=7; i<20; i++)); do
+    I=$(printf %02i $i)
+    sudo wget http://data.astrometry.net/4100/index-41$I.fits
+done
+# index-5000-*
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    sudo wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5200-$I.fits
+done
+
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    sudo wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5201-$I.fits
+done
+
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    sudo wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5202-$I.fits
+done
